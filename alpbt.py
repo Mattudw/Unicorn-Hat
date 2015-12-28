044a7939e89ddb386820796e8891fa129a3bc1b3
Mattudw <mattudw@gmail.com>
2015-12-28 11:43:26 +0100

    Update alpbt.py

--- alpbt.py
+++ alpbt.py
@@ -1,3 +1,6 @@
+O = [O]
+W = [1]
+
 a = [
 [O,O,O,O],
 [O,O,O,O],
@@ -134,11 +137,154 @@ m = [
 [O,O,O,O,O,O],
 [O,O,O,O,O,O],
 [O,O,O,O,O,O],
-[O,O,O,O,O,O],
 [O,W,W,W,W,O],
 [W,O,w,O,W,O],
 [W,O,W,O,W,O],
+[O,O,O,O,O,O],
 [O,O,O,O,O,O]
+]
+
+n = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[O,W,W,O],
+[W,O,W,O],
+[W,O,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+o = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,W,W,O],
+[W,O,W,O],
+[W,W,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+p = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[O,W,W,O],
+[W,O,W,O],
+[W,W,W,O],
+[O,O,W,O],
+[O,O,W,O]
+]
+
+q = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,W,O,O],
+[W,O,W,O],
+[W,W,W,O],
+[W,O,O,O],
+[W,O,O,O]
+]
+
+r = [
+[O,O,O],
+[O,O,O],
+[O,O,O],
+[W,O,O],
+[O,W,O],
+[O,W,O],
+[O,O,O],
+[O,O,O]
+]
+
+s = [
+[O,O,O,O],
+[O,O,O,O],
+[W,W,W,O],
+[O,W,W,O],
+[W,O,O,O],
+[W,W,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+t = [
+[O,W,O],
+[O,W,O],
+[W,W,O],
+[O,W,O],
+[O,W,O],
+[W,W,O],
+[O,O,O],
+[O,O,O]
+]
+
+u = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,O,W,O],
+[W,O,W,O],
+[W,W,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+v = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,O,W,O],
+[W,O,W,O],
+[O,W,O,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+w = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,O,W,O],
+[W,W,W,O],
+[W,O,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+x = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,O,W,O],
+[O,W,O,O],
+[W,O,W,O],
+[O,O,O,O],
+[O,O,O,O]
+]
+
+y = [
+[O,O,O,O],
+[O,O,O,O],
+[O,O,O,O],
+[W,O,W,O],
+[W,O,W,O],
+[W,W,O,O],
+[W,O,O,O],
+[O,W,O,O]
+]
+
+z = [
+[O,O,O,O],
+[O,O,O,O],
+[W,W,W,O],
+[W,W,O,O],
+[O,O,W,O],
+[W,W,W,O],
+[O,O,O,O],
+[O,O,O,O]
 ]
 
 Exemple = [
